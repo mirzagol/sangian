74274ee23ae078cc6e7cf093bd72556fabc4cc7e
Mirzagol <mirzagol_ho@outlook.com>
2025-04-14 19:38:08 +0330

    Build grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
-
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-return <Button>Click me</Button>
+  const isLg = useBreakpointValue({ base: false, lg: true });
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "side main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show when={isLg}>
+        <GridItem area="side" bg="tomato">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
