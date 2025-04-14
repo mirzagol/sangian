8eb4456661a9f6b30c8dbb785e4edc3097db50a8
Mirzagol <mirzagol_ho@outlook.com>
2025-04-14 21:29:07 +0330

    Dark mode switcher

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,23 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Text, Spacer } from "@chakra-ui/react";
 import "@fontsource/vazirmatn";
+import { ColorModeButton } from "../components/ui/color-mode";
 
-const Navbar = () => {
+export default function Navbar({ children }: { children: React.ReactNode }) {
   return (
-    <HStack>
-      <Image
-        src="https://www.freepnglogos.com/uploads/discord-logo-png/discord-logo-png-1.png"
-        alt="Logo"
-        boxSize="50px"
-      />
-      <Text fontFamily="Vazirmatn" fontSize="lg" fontWeight={"bold"}>
-        مبلمان سنگیان
-      </Text>
-    </HStack>
+    <>
+      <HStack width="100%" padding="10px">
+        <Image
+          src="https://www.freepnglogos.com/uploads/discord-logo-png/discord-logo-png-1.png"
+          alt="Logo"
+          boxSize="50px"
+        />
+        <Text fontFamily="Vazirmatn" fontSize="lg" fontWeight="bold">
+          مبلمان سنگیان
+        </Text>
+        <Spacer />
+        <ColorModeButton />
+      </HStack>
+      {children}
+    </>
   );
-};
-
-export default Navbar;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import Layout from "./theme.tsx"; // Import the Layout component
 import { Provider } from "./components/ui/provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <Layout>
+        <App />
+      </Layout>
     </Provider>
   </StrictMode>
 );
