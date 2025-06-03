1045100f77409f5841f89892e1b099a701b31dfc
Mirzagol <mirzagol_ho@outlook.com>
2025-06-03 14:06:47 +0330

    feat: Update Navbar component with responsive layout and navigation functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,45 @@
-import { HStack, Image, Text, Spacer } from "@chakra-ui/react";
-import logo from "../assets/logo.webp"; // Adjust the path as necessary
+import {
+  HStack,
+  Image,
+  Text,
+  Spacer,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar({ children }: { children: React.ReactNode }) {
+  const navigate = useNavigate();
+  // On mobile: left, on desktop: right
+  const justify = useBreakpointValue({ base: "flex-start", md: "flex-end" });
+  const direction = useBreakpointValue({ base: "row", md: "row-reverse" });
+
   return (
     <>
-      <HStack width="100%" padding="10px">
-        <Image src={logo} alt="Logo" boxSize="50px" />
-        <Text fontSize="lg" fontWeight="bold">
-          مبلمان سنگیان
-        </Text>
-        <Spacer />
+      <HStack
+        width="100%"
+        padding="10px"
+        justifyContent={justify}
+        flexDirection={direction}
+      >
+        <HStack cursor="pointer" onClick={() => navigate("/")}>
+          <Text
+            fontSize="lg"
+            fontWeight="bold"
+            display={{ base: "none", lg: "block" }}
+          >
+            مبلمان سنگیان
+          </Text>
+          <Image src={logo} alt="Logo" boxSize="50px" />
+          <Text
+            fontSize="lg"
+            fontWeight="bold"
+            display={{ base: "block", lg: "none" }}
+          >
+            مبلمان سنگیان
+          </Text>
+        </HStack>
+        <Spacer display={{ base: "none", lg: "block" }} />
       </HStack>
       {children}
     </>
